ac11d2488bd443688d652a772431ed906d891a79
yamako-maxq <bbex31@gmail.com>
2026-04-06 19:41:20 +0900

    Update:Loaderのテストコード作成

---
+++ src/components/loader/tests/container.test.tsx
@@ -0,0 +1,26 @@
+import "@testing-library/jest-dom";
+import { render, screen, } from "@/utils/tests/render";
+import LoaderContainer from "../loaderContainer";
+
+describe("LoaderPresenter", () => {
+
+    test("ローディングが表示されているか", async () => {
+        render(
+            <LoaderContainer color="blue" />
+        );
+
+        // 詳細が表示されているか
+        expect(screen.getByTestId("loader"))
+            .toBeInTheDocument()
+    });
+
+    test("色の情報を渡さなくてもローディングが表示されているか", async () => {
+        render(
+            <LoaderContainer />
+        );
+
+        // 詳細が表示されているか
+        expect(screen.getByTestId("loader"))
+            .toBeInTheDocument()
+    });
+});
